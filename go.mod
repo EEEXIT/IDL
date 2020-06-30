8ac7afd7c9f83a70e00baf10062b38b1b36a6377
이신일 <gocheat@hyundai-autoever.com>
2020-06-30 18:07:27 +0900

    proto gateway 추가

--- go.mod
+++ go.mod
@@ -3,12 +3,13 @@ module github.com/EEEXIT/IDL
 go 1.13
 
 require (
+	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b
 	github.com/golang/protobuf v1.4.2
-	github.com/google/go-cmp v0.4.0
-	golang.org/x/net v0.0.0-20200602114024-627f9648deb9 // indirect
+	github.com/grpc-ecosystem/grpc-gateway v1.14.6
+	golang.org/x/net v0.0.0-20200602114024-627f9648deb9
 	golang.org/x/sys v0.0.0-20200622214017-ed371f2e16b4 // indirect
 	golang.org/x/text v0.3.3 // indirect
-	google.golang.org/genproto v0.0.0-20200623002339-fbb79eadd5eb // indirect
-	google.golang.org/grpc v1.30.0 // indirect
-	google.golang.org/protobuf v1.24.0
+	google.golang.org/genproto v0.0.0-20200626011028-ee7919e894b5
+	google.golang.org/grpc v1.30.0
+	google.golang.org/protobuf v1.25.0
 )
--- protos/gencode.go
+++ protos/gencode.go
@@ -1,4 +1,8 @@
-//go:generate protoc -I. -I../vendor -I$GOPATH/src --go_opt=paths=source_relative --go_out=plugins=grpc:../generated/go --go_opt=paths=source_relative   ./meeting/meeting.proto ./meeting/auth.proto ./utils/paginator.proto
-//go:generate protoc -I. --proto_path=../  --dart_out=../generated/dart   ./meeting/meeting.proto ./utils/paginator.proto
+// Golang
+//go:generate protoc -I. -I./googleapis -I../vendor --go_opt=paths=source_relative --go_out=plugins=grpc:../generated/go --go_opt=paths=source_relative ./meeting/meeting.proto ./meeting/auth.proto ./utils/paginator.proto
+//go:generate protoc -I. -I./googleapis -I../vendor  --grpc-gateway_out=logtostderr=true,paths=source_relative,allow_delete_body=true:../generated/go ./meeting/meeting.proto ./meeting/auth.proto
+
+// Dart
+//go:generate protoc -I. --proto_path=../  --dart_out=../generated/dart ./meeting/meeting.proto ./utils/paginator.proto
 
 package protos
